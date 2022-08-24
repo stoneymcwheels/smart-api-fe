d2eace8564609400660ace45dc10df32bdacd4b1
stoneymcwheels <stoneymcwheels@protonmail.com>
2022-08-24 19:37:15 -0400

    cors issue fixed

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://arcane-wave-61194.herokuapp.com/imageurl', {
+      fetch('https://cors-anywhere.herokuapp.com/https://arcane-wave-61194.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('https://arcane-wave-61194.herokuapp.com:3000/image', {
+          fetch('https://cors-anywhere.herokuapp.com/https://arcane-wave-61194.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://arcane-wave-61194.herokuapp.com/register', {
+    fetch('https://cors-anywhere.herokuapp.com/https://arcane-wave-61194.herokuapp.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
